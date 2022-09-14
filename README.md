9bc637c7a106d4a5edcaa928ea52f2ca2e2fc137
Devang K <52554056+rising-entropy@users.noreply.github.com>
2022-09-14 16:04:51 +0530

    decrypt railfence

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # CNS-Lab-Archives
+
+CNS Lab Assignments:
+* event based input
+- Caesar
+- Cryptanalysis -> find meaningful out of dictionary
+- Playfair
+- Vignere Cipher
+- Transposition Cipher - Railfence
+- Transposition Cipher - Columnar
+- DES Algo - Part (a) - Just I/P O/P through VLab
+- DES Algo - Part (b) - Implementation
+- AES Algo - Part (a) - Just I/P O/P through VLab
+- AES Algo - Part (b) - Implementation
---
+++ TranspositionCipher/RailfenceCipher/decrypt.py
@@ -0,0 +1,71 @@
+import math
+
+def printMatrix(fenceMatrix):
+    for i in range(len(fenceMatrix)):
+        for j in range(len(fenceMatrix[0])):
+            print(fenceMatrix[i][j], end='')
+        print()
+
+encryptedText = "LOIHANODNRDEAFLEXNBGSLX"
+rails_count = 3
+
+fenceMatrix = []
+# No. of Rows -> No. of Rails
+# No. of Cols -> Size of encryptedText + No. Rails
+
+noOfCols = len(encryptedText) + rails_count
+
+for i in range(rails_count):
+    individualFence = []
+    for j in range(noOfCols):
+        individualFence.append("-")
+    fenceMatrix.append(individualFence)
+
+# We first mark the indices with '*'
+rowIndex = 0
+colIndex = 0
+slopeDown = True
+for c in range(len(encryptedText)):
+    fenceMatrix[rowIndex][colIndex] = '*'
+    if slopeDown:
+        rowIndex += 1
+        if (rowIndex+1) > rails_count:
+            slopeDown = False
+            rowIndex -= 2
+    else:
+        rowIndex -= 1
+        if (rowIndex) < 0:
+            slopeDown = True
+            rowIndex += 2
+    colIndex += 1
+
+# Replace * with the Encrypted Text
+encTextIndex = 0
+for i in range(rails_count):
+    for j in range(noOfCols):
+        if fenceMatrix[i][j] == '*':
+            fenceMatrix[i][j] = encryptedText[encTextIndex]
+            encTextIndex += 1
+
+plaintext = ''
+
+rowIndex = 0
+colIndex = 0
+slopeDown = True
+# Now we iterate in the rails and create the plain-text
+for c in range(len(encryptedText)):
+    plaintext+=fenceMatrix[rowIndex][colIndex]
+    if slopeDown:
+        rowIndex += 1
+        if (rowIndex+1) > rails_count:
+            slopeDown = False
+            rowIndex -= 2
+    else:
+        rowIndex -= 1
+        if (rowIndex) < 0:
+            slopeDown = True
+            rowIndex += 2
+    colIndex += 1
+
+print("Encrypted Text:", encryptedText)
+print("Plaintext:", plaintext)
